44ec9ebc408cbc1c311729c1ddb049025f09d459
Ken Moore <moorekou@gmail.com>
2016-02-03 15:32:38 -0500

    Add the ability to use the "-localhost" CLI flag on FreeBSD for launching a stand-alone client for the local system (automatically logged in as the current user). This will be what a "Control Panel" shortcut should run on PC-BSD/FreeBSD systems.

--- src-qt5/gui_client/main.cpp
+++ src-qt5/gui_client/main.cpp
@@ -9,6 +9,13 @@
 
 #include "globals.h"
 #include "TrayUI.h"
+#include "mainUI.h"
+
+#ifdef __FreeBSD__
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#endif
 
 //Initialize the global variables (defined in globals.h)
 //sysadm_client *S_CORE = new sysadm_client();
@@ -18,30 +25,49 @@ int main( int argc, char ** argv )
 {
   //Load the application
   QApplication A(argc, argv);
-    A.setQuitOnLastWindowClosed(false); //the tray icon is not considered a window
+    
+  //Determine if this is a stand-alone instance of the client for the localhost	
+  bool local_only = false;
+  for(int i=1; i<argc; i++){
+    #ifdef __FreeBSD__
+    if(QString(argv[i])=="-localhost"){ local_only = true; break;}
+    #endif
+  }
+  int ret = 0; //return code
+  if(!local_only){
   //Wait a bit until a system tray is available
-  bool ready = false;
-  for(int i=0; i<60 && !ready; i++){
-    ready = QSystemTrayIcon::isSystemTrayAvailable();
-    if(!ready){
-      //Pause for 5 seconds
-     A.thread()->usleep(5000000); //don't worry about stopping event handling - nothing running yet
+    A.setQuitOnLastWindowClosed(false); //the tray icon is not considered a window
+    bool ready = false;
+    for(int i=0; i<60 && !ready; i++){
+      ready = QSystemTrayIcon::isSystemTrayAvailable();
+      if(!ready){
+        //Pause for 5 seconds
+       A.thread()->usleep(5000000); //don't worry about stopping event handling - nothing running yet
+      }
     }
-  }
-  if(!ready){
-    qDebug() << "Could not find any available system tray after 5 minutes: exiting....";
-    return 1;
-  }
-  //Now start up the system tray
-  sysadm_tray *T = new sysadm_tray();
+    if(!ready){
+      qDebug() << "Could not find any available system tray after 5 minutes: exiting....";
+      return 1;
+    }
+    //Now start up the system tray
+    sysadm_tray *T = new sysadm_tray();
     T->show();
-  //Start the event loop
-  int ret = A.exec();
+    //Start the event loop
+    ret = A.exec();
+    
+  }else{
+    //Open the stand-alone client just for the localhost
+    sysadm_client CORE;
+      CORE.openConnection(getlogin(),"","127.0.0.1");
+    MainUI M(&CORE);
+    M.show();
+    ret = A.exec();
+  }
+  
+  
   //Clean up any global classes before exiting
-  //S_CORE->closeConnection();
   settings->sync();
   //Now fully-close the global classes
   delete settings;
-  //delete S_CORE;
   return ret;
 }
